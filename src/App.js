7dfae3377a006d26d48536f506fbf0e9f09fb5bd
Satish Konda <satishkondamca@gmail.com>
2021-05-19 00:50:55 +0100

    Initial Commit for Movie List app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Search from './components/Search';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+       <h1>Movie Database</h1>
+      </header>
+      <main>
+        <Search/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
