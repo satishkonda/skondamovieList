03d0215f990c93fdae6bf90bd205ef8581b701bd
Satish Konda <satishkondamca@gmail.com>
2021-05-21 17:18:17 +0100

    Adding handle event to Search component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Search from './components/Search';
 
 function App() {
+  const apiUrl = "http://localhost:3000/movies";
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {}
+  });
+
+  const search = (e) => {
+    if(e.key === "Enter"){
+      console.log('Fired on enter key');
+    }
+  }
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+    setState(prevState =>{
+      return {...prevState, s:s}
+    });
+    console.log(state.s);
+  }
   return (
     <div className="App">
       <header>
        <h1>Movie Database</h1>
       </header>
       <main>
-        <Search/>
+        <Search handleInput={handleInput}/>
       </main>
     </div>
   );
